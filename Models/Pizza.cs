7f3950324fd9d6542679a96b686446f98ba81597
Hycarus <mirkomarasco08@gmail.com>
2024-05-10 15:45:28 +0200

    exercise completed

---
+++ Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace la_mia_pizzeria_static.Models
+{
+	public class Pizza
+	{
+		public string Name { get; set; }
+		public string Overview { get; set; }
+		public string Image { get; set; }
+		public double Price { get; set; }
+
+		public Pizza(string name, string overview, string image, double price)
+		{
+			this.Name = name;
+			this.Overview = overview;
+			this.Image = image;
+			this.Price = price;
+		}
+	}
+}
+
